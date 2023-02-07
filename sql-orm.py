cc9127e68f1f5ab1ca9dc4ff6ae451874e73619f
Monica Murray <Monica.rickerby@gmail.com>
2023-02-07 21:26:47 +0000

    Practice CRUD by adding file sql-crud.py

--- sql-orm.py
+++ sql-orm.py
@@ -6,3 +6,16 @@ from sqlalchemy.ext.declarative import declarative_base
 
 from sqlalchemy.orm import sessionmaker
 
+db = create-engine("postgreSQL:///chinook")
+base = declarative_base()
+
+Session = sessionmaker(db)
+
+session = Session()
+
+base.metadata.create_all(db)
+
+#Now that we have the file set up, we can start to build our class-based models.
+#These will be quite similar to how we did them in the last lesson, but this time, we
+#get to simply build a normal Python object, that subclasses 'base'.
+
